b95321fb3c87e430fce6bff87eca8a2f77c6d9ef
jennymarie1027 <jennymarie1027@gmail.com>
2024-04-04 16:38:40 -0600

    favorites listed

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -5,29 +5,31 @@ import { Post } from 'src/models/Post';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent {
   email = '';
-  faveoriteList:any
-  favorites:any = []
-  constructor(private authService: AuthService, private postService: PostService) {}
+  faveoriteList: any;
+  favorites: any = [];
+  constructor(
+    private authService: AuthService,
+    private postService: PostService
+  ) {}
 
   ngOnInit() {
-    this.getUserEmail()
-    this.getListOfFaves()
-    this.getListOfFaves()
+    this.getUserEmail();
+    this.getListOfFaves();
+    this.getListOfFaves();
   }
 
-  getUserEmail(){
-    this.authService.email$.subscribe((email) => this.email = email)
+  getUserEmail() {
+    this.authService.email$.subscribe((email) => (this.email = email));
   }
 
-  getListOfFaves(){
-    const userId = localStorage.getItem('user_id')
-    this.postService.getFaves(userId).subscribe((res) => {
-      console.log('list of faves = ', res)
-      this.favorites = res
-    })
+  getListOfFaves() {
+    const userId = localStorage.getItem('user_id');
+    this.postService
+      .getFaves(userId)
+      .subscribe((res) => (this.favorites = res));
   }
 }
