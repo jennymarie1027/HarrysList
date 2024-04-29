e435b4e146a0ee20ce3225fb1d7e2b9d2ea540a6
jennymarie1027 <jennymarie1027@gmail.com>
2024-04-29 15:07:11 -0600

    mat checkboxes

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { categories } from 'src/models/Categories';
+import {FormBuilder, FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,5 +8,27 @@ import { categories } from 'src/models/Categories';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
-  cats = categories
+  categories = categories
+  formattedCategories: any
+  constructor(private formBuilder: FormBuilder){}
+
+  ngOnInit(){
+    let form = this.formatForm()
+    this.formattedCategories = this.formBuilder.group(form)
+  }
+
+  formatForm(){
+    let justCategories = this.categories.map((cat) => {
+      return cat.value
+    })
+    let form = {}
+    for(let i = 0; i < justCategories.length; i++) {
+      form[justCategories[i]] = false
+    }
+    console.log('form = ', form)
+    return form;
+  }
+
+  // this.categories = this.formBuilder.group(this.cats)
+
 }
